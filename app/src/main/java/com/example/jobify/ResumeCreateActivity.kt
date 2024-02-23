5d056e804253e53115f6b0bbbb2f186780aae2ca
Zhardem Gumarov <gumarov2611@gmail.com>
2024-02-23 14:06:25 +0600

    Zhardem

---
+++ app/src/main/java/com/example/jobify/ResumeCreateActivity.kt
@@ -0,0 +1,11 @@
+package com.example.jobify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResumeCreateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.resume_create_page)
+    }
+}
